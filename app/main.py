817197e151372c8155fdd579dc7b0ea842ce33b1
samueldsr8 <samueldsr8@gmail.com>
2022-02-12 00:35:45 -0500

    feat: Add `CORS` middleware

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from popdyn import (
     Model,
     Transition
 )
 
+origins = ["*"]
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class TransitionRequest(BaseModel):
